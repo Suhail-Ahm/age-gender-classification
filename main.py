bd22b72975b384c1d68e61c59efc6855719dda1c
Suhail Ahmed <suhail.a@icubeswire.com>
2025-05-12 14:56:37 +0530

    changes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.post("/detect-gender")
 async def detect_gender(file: UploadFile = File(...)):
-    with open("temp.jpg", "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    with open("temp.jpg", "wb") as f:
+        shutil.copyfileobj(file.file, f)
 
     result = DeepFace.analyze(img_path="temp.jpg", actions=["gender"])
     return {
